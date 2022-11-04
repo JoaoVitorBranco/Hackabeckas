0e253541f8cef29c12bb053f0a31e8ff18b00e84
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-11-04 20:40:45 -0300

    EH O DIMAS POHA 😎

---
+++ src/modules/create_order/app/create_order_presenter.py
@@ -0,0 +1,16 @@
+from src.shared.helpers.http.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from src.modules.create_order.app.create_order_usecase import CreateOrderUsecase
+from src.modules.create_order.app.create_order_controller import CreateOrderController
+from src.shared.infra.repositories.hackabeckas_repository_mock import HackabeckasRepositoryMock
+
+def lambda_handler(event, context):
+    repo = HackabeckasRepositoryMock()
+    usecase = CreateOrderUsecase(repo)
+    controller = CreateOrderController(usecase)
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(
+        status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
