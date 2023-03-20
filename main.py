887bb9f0a95183b15a7756052d764f145238559b
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-03-19 22:35:00 -0300

    post method requires data

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 from src.modules.create_order.app.create_order_presenter import create_order_presenter
 from src.modules.get_order.get_order_presenter import get_order_presenter
 
 app = FastAPI()
 
 @app.get("/get_order/")
-def get_order(id_order: int = None):
+def get_order(id_order:int = None):
     request = {
         "body":{},
         "headers": {},
         "query_params": {
-            "id_order": id_order
+            "id_order": str(id_order)
         }
     }
     response = get_order_presenter(request, None)
     return response
 
 @app.post("/create_order/")
-async def create_order(request: Request):
-    body = await request.json()
-    request_json = {
-        "body":body
+def create_order(data: dict = None):
+    event = {
+        "body":data
     }
     
-    response = create_order_presenter(request_json, None)
+    response = create_order_presenter(event, None)
     return response
